903e69fa5a12cb03a8dc0000924b8154d78b2f92
George Navas <gannavas@gmail.com>
2015-03-31 12:08:07 -0700

    Crawler work

--- app/controllers/crawlers_controller.rb
+++ app/controllers/crawlers_controller.rb
@@ -1,29 +1,35 @@
 class CrawlersController < ApplicationController
 
-  def create
-    # STARTER CODE FOR SIDEKIQ
-    # @crawler = Crawler.new
-    # CrawlerWorker.perform_in(4.hour@crawler.id)
+  # def create
+  #   # STARTER CODE FOR SIDEKIQ
+  #   # @crawler = Crawler.new
+  #   # CrawlerWorker.perform_in(4.hour@crawler.id)
 
-    # VARIABLES WE NEED TO PASS INTO THE URI, WILL COME FROM SELECTORS ON QUEUE VIEWS
-    restaurants = ["bourbon-steak-san-francisco"]
-    dates = ["2015-04-04"]
-    times = ["2122"]
-    covers = ["2"]
+  #   # VARIABLES WE NEED TO PASS INTO THE URI, WILL COME FROM SELECTORS ON QUEUE VIEWS
+  #   restaurants = ["bourbon-steak-san-francisco"]
+  #   dates = ["2015-04-04"]
+  #   times = ["2122"]
+  #   covers = ["2"]
 
-    # URI WE ARE USING TO HIT OPEN TABLE
-    url = "http://www.opentable.com/#{restaurants}?DateTime=#{dates}%#{times}&Covers=#{covers}"
+  #   # URI WE ARE USING TO HIT OPEN TABLE
+  #   url = "http://www.opentable.com/#{restaurants}?DateTime=#{dates}%#{times}&Covers=#{covers}"
 
-    # NOKOGIRI METHOD; WILL RETURN AVAILABLE TIMES AT THE SPECIFIED RESTAURANT AS A STRING
-    doc = Nokogiri::HTML(open(url))
-    times = doc.css("a.dtp-button.button").text.split(" PM")
-    times.each do |time|
-      puts time.delete(" ").to_s
-    end
+  #   # NOKOGIRI METHOD; WILL RETURN AVAILABLE TIMES AT THE SPECIFIED RESTAURANT AS A STRING
+  #   doc = Nokogiri::HTML(open(url))
+  #   times = doc.css("a.dtp-button.button").text.split(" PM")
+  #   times.each do |time|
+  #     puts time.delete(" ").to_s
+  #   end
 
-    # REMAINING WORK
-    # 1 – Merge times and dates into a hash
-    # 2 - Run validations of available resturant times against user's requests (should this be in this controller or should we do it in another controller?)
+  #   # REMAINING WORK
+  #   # 1 – Merge times and dates into a hash
+  #   # 2 - Run validations of available resturant times against user's requests (should this be in this controller or should we do it in another controller?)
+  # end
+
+  def run
+    cue_restaurants = CueRestaurant.all
+    Crawler.crawler_check cue_restaurants
+    redirect_to login_path
   end
 
 end
