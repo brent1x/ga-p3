763a180b65e9d42a2406e947eb9c63f9c7fffda6
George Navas <gannavas@gmail.com>
2015-03-31 17:11:06 -0700

    overriding sites controller

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -23,23 +23,14 @@ class SitesController < ApplicationController
   #add a restaurant to the rest. database
   def add_restaurant
   restaurant_info = Open.find_restaurant params[:restaurant][:name], params[:restaurant][:city], params[:restaurant][:state]
-  @restaurant = Restaurant.create restaurant_params
-    if @restaurant.save
+    if restaurant_info
       message = Crawler.url_check  restaurant_info
       message
-
     else
-    flash[:notice] = "Restaurant not found" 
+    flash[:notice] = "Restaurant not found"
     end
-    # redirect_to add_path
-    redirect_to home_path
-  end
-private
-  def restaurant_params
-
-    params.require(:restaurant).permit(:name, :city, :state)
+    redirect_to add_path
   end
 
 
 end
-#www.opentable.com/bourbon-steak-san-francisco?DateTime=2015-04-06%2122&Covers=2
