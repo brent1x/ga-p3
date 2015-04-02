5b3dd6520c6279d3a784b9c15c92423dfd2989c3
George Navas <gannavas@gmail.com>
2015-04-02 10:40:28 -0700

    fix

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -131,7 +131,7 @@ user_join_table.each do |user_join_table_row|
 user_join_table_row.shift
 first_available = user_join_table_row[0].first
 restaurant_name[counter] = Restaurant.find(first_available.restaurant_id).name
-puts final_hash
+puts "this is the final has #{final_hash}"
 puts final_hash[Restaurant.find(first_available.restaurant_id).name]
 available_date[counter] = final_hash[Restaurant.find(first_available.restaurant_id).name].keys.first
 available_time[counter] = final_hash[Restaurant.find(first_available.restaurant_id).name][available_date[counter]].first
